b7a56398ae70f7ffb87d6a7c62b324842a605d71
Alexey Potapov <2furius@gmail.com>
2022-08-07 09:52:31 +0300

    Начальная подготовка базового шаблона и временный его вывод во вьюхе индекса

--- web_board/views.py
+++ web_board/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render
 
 from telegram_group_poster.utils import send_message
 
 
 def index(request):
-    return HttpResponse("Hello")
+    return render(request, "base.html")
 
 
 def send(request):
